af6e0d6efcc573510537fa055a941ebbb2b44813
Paweloronowicz <oronowicz@gmail.com>
2024-12-11 21:08:58 +0100

    Calculation for Sulfuras item

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -7,33 +7,30 @@ final class GildedRose
     const MAX_QUALITY = 50;
     const LEGENDARY_QUALITY = 80;
     
-    public function updateQuality($item)
+    public function updateQuality($item): void
     {
         $is_common_item = true;
 
         if($item->name === 'Aged Brie') {
             $is_common_item = false;
+            $this->calculateAgedBrie($item);
         }
 
         if($item->name === 'Backstage passes to a TAFKAL80ETC concert') {
             $is_common_item = false;
+            $this->calculateBackstagePasses($item);
         }
 
         if($item->name === 'Sulfuras, Hand of Ragnaros') {
             $is_common_item = false;
+            $this->calculateSulfuras($item);
         }
 
         if($is_common_item) {
             $this->calculateCommonItem($item);
         }
 
-        if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if ($item->quality > 0) {
-                if ($item->name === 'Sulfuras, Hand of Ragnaros') {
-                    $item->quality = 80;
-                }
-            }
-        } else {
+        if ($item->name === 'Aged Brie' || $item->name === 'Backstage passes to a TAFKAL80ETC concert') {
             if ($item->quality < 50) {
                 $item->quality = $item->quality + 1;
                 if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
@@ -68,16 +65,43 @@ final class GildedRose
         }
     }
 
-    public function calculateCommonItem(Item $item) {
+    /**
+     * Calculate quality and sell-in for a common item
+     * @param Item $item
+     * @return void
+     */
+    public function calculateCommonItem(Item $item): void
+    {
         $item->sell_in--;
 
         if($item->quality !== 0) {
             $item->quality--;
 
+            // after expiry date
             if($item->sell_in < 0 && $item->quality > 0) {
                 $item->quality--;
             }
         }
+    }
+
+    public function calculateBackstagePasses(Item $item): void
+    {
+
+    }
+
+    /**
+     * Calculate quality for static Sulfuras item
+     * @param Item $item
+     * @return void
+     */
+    public function calculateSulfuras(Item $item): void
+    {
+        $item->quality = self::LEGENDARY_QUALITY;
+    }
+
+    public function calculateAgedBrie(Item $item): void
+    {
+
     }
 
 }
